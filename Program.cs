65dde500e95593b25e078dbaead5f74ce2172f07
konstantin <ognev.k.a@mail.ru>
2022-07-29 21:37:20 +0300

    add MainTask

--- Program.cs
+++ Program.cs
@@ -17,15 +17,23 @@ int CountElements(string[] array)
 {
 	int count = 0;
 	for (int i = 0; i < array.Length; i++)
-	{
 		if (array[i].Length <= 3) count++;
-	}
 	return count;
+}
+
+string[] MainTask(string[] array, int count)
+{
+	int index = 0;
+	string[] outputArray = new string[count];
+	for (int i = 0; i < array.Length; i++)
+		if (array[i].Length <= 3) outputArray[index++] = array[i];
+	return outputArray;
 }
 
 Console.Write("Введите длину массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
-string[] array = CreateArray(size);
-Words(array);
-int count = CountElements(array);
+string[] inputArray = CreateArray(size);
+Words(inputArray);
+int count = CountElements(inputArray);
 Console.WriteLine(count);
+string[] outputArray = MainTask(inputArray, count);
