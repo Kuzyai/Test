8b43c0166a40e6c58e4634a3cf7e8d1d10ed211a
konstantin <ognev.k.a@mail.ru>
2022-07-29 21:25:08 +0300

    add CountElements

--- Program.cs
+++ Program.cs
@@ -11,10 +11,21 @@ void Words(string[] array)
 		Console.Write($"Введите {i + 1} слово: ");
 		array[i] = Console.ReadLine();
 	}
-	// return array;
+}
+
+int CountElements(string[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i].Length <= 3) count++;
+	}
+	return count;
 }
 
 Console.Write("Введите длину массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] array = CreateArray(size);
 Words(array);
+int count = CountElements(array);
+Console.WriteLine(count);
