17aa4ce13413fe253ce91d426f78e2f2eaa01ac9
konstantin <ognev.k.a@mail.ru>
2022-07-29 21:18:29 +0300

    add Words

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 	string[] array = new string[size];
 	return array;
 }
+
+void Words(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"Введите {i + 1} слово: ");
+		array[i] = Console.ReadLine();
+	}
+	// return array;
+}
+
+Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(size);
+Words(array);
